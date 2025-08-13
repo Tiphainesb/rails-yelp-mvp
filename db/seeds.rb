63243dd1d817b1c8fb59e549d748506dca95354a
Tiphaine deSaint-Bon <b00756200@essec.edu>
2025-08-13 16:16:45 +0200

    mm

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "cleaning database..."
+Restaurant.destroy_all
+
+puts "creating restaurants..."
+5.times do
+  Restaurant.create!(
+  name: Faker::Restaurant.name,
+  address: Faker::Address.full_address,
+  phone_number: Faker::PhoneNumber.phone_number,
+  category: %w[chinese italian japanese french belgian].sample
+  )
+end
+
+puts "Finished!"
